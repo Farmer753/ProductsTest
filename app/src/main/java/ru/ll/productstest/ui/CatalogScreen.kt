43a18b28f6cbf0727756430d5fd6dc316e8726fd
LL <ludo4ka21@yandex.ru>
2024-05-08 00:03:16 +0300

    start screen catalog layout

--- app/src/main/java/ru/ll/productstest/ui/CatalogScreen.kt
+++ app/src/main/java/ru/ll/productstest/ui/CatalogScreen.kt
@@ -4,11 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,5 +64,50 @@ fun Catalog() {
                 )
             }
         }
+        val selected = remember {
+            mutableStateOf(false)
+        }
+        SelectableButton(selected = selected.value, text = "Роллы") {
+            selected.value = selected.value.not()
+        }
+        Box(modifier = Modifier.padding(16.dp, 12.dp)) {
+            Button(
+                onClick = { },
+                modifier = Modifier.fillMaxWidth(),
+                shape = MaterialTheme.shapes.medium,
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.cart),
+                    contentDescription = "cart"
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(
+                    text = "2 160 Р",
+                    color = Color.White
+                )
+            }
+
+        }
+    }
+
+
+}
+
+@Composable
+fun SelectableButton(
+    selected: Boolean,
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        colors = if (selected) {
+            ButtonDefaults.buttonColors()
+        } else {
+            ButtonDefaults.textButtonColors()
+        }
+    ) {
+        Text(text = text)
     }
 }
