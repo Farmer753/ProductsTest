f7ebba12f8efe0fb559fa505d8d76d978a05ad1e
LL <ludo4ka21@yandex.ru>
2024-07-02 23:47:46 +0300

    ProductScreen in progress

--- app/src/main/java/ru/ll/productstest/ui/product/ProductScreen.kt
+++ app/src/main/java/ru/ll/productstest/ui/product/ProductScreen.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
@@ -20,7 +23,6 @@ import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -143,7 +145,7 @@ fun ProductDetailsView() {
                 .wrapContentHeight()
 
         )
-//        Box(modifier = Modifier.padding(8.dp)){
+        Box(modifier = Modifier.padding(8.dp)) {
             Text(
                 text = "2 500 P",
                 style = MaterialTheme.typography.body1,
@@ -151,22 +153,23 @@ fun ProductDetailsView() {
                     .wrapContentWidth()
                     .wrapContentHeight()
             )
-//        }
-//            Canvas(modifier = Modifier.fillMaxSize()) {
-//
-//                // Fetching width and height for
-//                // setting start x and end y
-//                val canvasWidth = size.width
-//                val canvasHeight = size.height
-//
-//                // drawing a line between start(x,y) and end(x,y)
-//                drawLine(
-//                    start = Offset(x = canvasWidth, y = 0f),
-//                    end = Offset(x = 0f, y = canvasHeight),
-//                    color = Color.Red,
-//                    strokeWidth = 5F
-//                )
-//            }
+
+            Canvas(modifier = Modifier.size(20.dp)) {
+
+                // Fetching width and height for
+                // setting start x and end y
+                val canvasWidth = size.width
+                val canvasHeight = size.height
+
+                // drawing a line between start(x,y) and end(x,y)
+                drawLine(
+                    start = Offset(x = canvasWidth, y = 0f),
+                    end = Offset(x = 0f, y = canvasHeight),
+                    color = Color.Red,
+                    strokeWidth = 5F
+                )
+            }
+        }
     }
 }
 
